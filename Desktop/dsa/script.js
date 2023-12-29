d447318f269cac9c60cd03a643b7cd5adf9e3374
Krystle Mitchell <mitchell.krystle2@gmail.com>
2023-12-29 13:49:50 -0800

    Test Commit

---
+++ Desktop/dsa/script.js
@@ -0,0 +1,11 @@
+const s = [
+    ['John Doe', 20, 60, 'A'],
+    ['Jane Doe', 10, 52, 'B'],
+    ['Petr Chess', 5, 24, 'F'],
+    ['Ling Jess', 28, 43, 'A'],
+    ['Ben Liard', 16, 51, 'B']
+]
+console.table(s)
+
+
+
